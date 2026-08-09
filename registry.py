4806feec23b6f27fcfb81fe41933049356269eec
goko22 <steamyazsalexd@gmail.com>
2026-08-09 13:02:44 +0300

    Delete registry.py

--- registry.py
+++
@@ -1,9 +0,0 @@
-from .generic import GenericScraper
-
-# İlk sürüm: bütün siteler GenericScraper üzerinden çalışır.
-# Sonraki adımda mağaza bazlı özel scraper'lar bu registry'e eklenecek.
-SCRAPER_REGISTRY = {}
-
-def get_scraper(store_name: str):
-    scraper_cls = SCRAPER_REGISTRY.get(store_name, GenericScraper)
-    return scraper_cls()
